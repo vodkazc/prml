ffd3509a47c71933dd5951f5413530089fbb9143
zhangchi <vodkazc@gmail.com>
2017-02-20 23:37:22 +0800

    20170220_2336 convert trs to numpy.array

--- TraceParser.py
+++ TraceParser.py
@@ -8,13 +8,6 @@ from struct import pack, unpack
 from ctypes import *
 
 
-# load C library
-try:
-    libc = cdll.msvcrt       # Windows
-except AttributeError:
-    libc = CDLL("libc.so.6")  # Linux
-
-
 class CommonFile(object):
     """Common File Object for IO"""
 
@@ -29,69 +22,47 @@ class CommonFile(object):
         if mode[0] == 'r':
             # in read mode
             self.fileHandler = open(self.path, mode)
-        else:
             # in write/append mode
-            libc.fopen.restype = c_void_p
-            libc.fopen.argtypes = c_char_p, c_char_p
-            self.fileHandlerC = libc.fopen(self.path.encode('utf-8'), mode.encode('utf-8'))
+
         # return True
         # else:
         #     logging.info(self.path + 'does not exist.')
         #     return False
 
-    def closeFile(self):
+    def closefile(self):
         if self.fileHandler:
             self.fileHandler.close()
             self.byteNum = 0
         return True
 
-    def closeFileC(self):
-        if self.fileHandlerC:
-            libc.fclose.argtypes = c_void_p,
-            libc.fclose.restype  = c_int
-            libc.fclose(self.fileHandlerC)
-            self.byteNum = 0
+
+    def writefile(self, point_list):
         return True
 
-    def writeFile(self, point_list):
-        libc.fwrite.argtypes = c_void_p ,c_size_t, c_size_t, c_void_p
-        libc.fwrite.restype  = c_size_t
-        # generate a c-style string buffer from python list
-        c_buf = (c_float*len(point_list))(*point_list)
-        n_items = libc.fwrite(c_buf, 4, len(point_list), self.fileHandlerC)
-        # print('points written: ',n_items, ', actual points: ',len(point_list))
+    def writebyte(self, byte_str):
         return True
 
-    def writeByte(self, byte_str):
-        libc.fwrite.argtypes = c_void_p ,c_size_t, c_size_t, c_void_p
-        libc.fwrite.restype  = c_size_t
-        # print(byte_str)
-        n_items = libc.fwrite(byte_str, 1, len(byte_str), self.fileHandlerC)
-        return True
-
-
-    def readByte(self, num):
+    def readbyte(self, num):
         byte_re = self.fileHandler.read(num)
         self.byteNum += num
         return byte_re
 
-    def readInt(self, num=4):
+    def readint(self, num=4):
         byte_re = self.fileHandler.read(num)
         self.byteNum += num
         return int.from_bytes(byte_re, 'little')
 
-    def readFloat(self, num=4):
+    def readfloat(self, num=4):
         byte_re = self.fileHandler.read(num)
         self.byteNum += num
         return float.fromhex(byte_re.hex())
 
-    def readStr(self, num):
+    def readstr(self, num):
         byte_re = self.fileHandler.read(num)
         self.byteNum += num
         return byte_re.decode()
 
-
-    def seekFile(self, num=0):
+    def seekfile(self, num=0):
         self.fileHandler.seek(num, 0)
         return True
 
@@ -124,136 +95,136 @@ class Trace(object):
         self.logScale = 0
         # self.parseTraceHeader()
 
-    def readHeaderDataLength(self):
-        data_length = self.traceFile.readInt(1)
+    def readheaderdatalength(self):
+        data_length = self.traceFile.readint(1)
         if data_length & 0x80:
             data_length &= 0x7F
-            data_length = self.traceFile.readInt(data_length)
+            data_length = self.traceFile.readint(data_length)
         return data_length
 
-    def parseTraceHeader(self):
+    def parsetraceheader(self):
         logging.info('Parsing Trace Header')
         self.traceFile.openfile('rb')
         while True:
-            ch = self.traceFile.readByte(1)
+            ch = self.traceFile.readbyte(1)
 
             if ch not in self.TraceSetObjects:
                 logging.error('Unknown Trace Header :' + ch.hex())
                 raise ValueError('Unknown Trace Header :' + ch.hex())
             if ch == b'\x5F':
                 logging.debug('Parsing Trace File End.')
-                self.readHeaderDataLength()
+                self.readheaderdatalength()
                 self.headerLength = self.traceFile.byteNum
                 logging.debug('Trace Header Length : ' + str(self.headerLength))
                 break
             if ch == b'\x41':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
+                data_length = self.readheaderdatalength()
                 if data_length != 4:
                     logging.error('Wrong trace header : ' + ch.hex())
                     raise ValueError('Wrong Trace Header')
-                self.traceNumber = self.traceFile.readInt(data_length)
+                self.traceNumber = self.traceFile.readint(data_length)
                 logging.debug('Trace Number : ' + str(self.traceNumber))
             if ch == b'\x42':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
+                data_length = self.readheaderdatalength()
                 if data_length != 4:
                     logging.error('Wrong trace header : ' + ch.hex())
                     raise ValueError('Wrong Trace Header')
-                self.pointCount = self.traceFile.readInt(data_length)
+                self.pointCount = self.traceFile.readint(data_length)
                 logging.debug('Point Count : ' + str(self.pointCount))
             if ch == b'\x43':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
+                data_length = self.readheaderdatalength()
                 if data_length != 1:
                     logging.error('Wrong Trace Header : ' + ch.hex())
                     raise ValueError('Wrong Trace Header')
-                value_tmp = self.traceFile.readInt(1)
+                value_tmp = self.traceFile.readint(1)
                 self.sampleCoding = (value_tmp & 0x10)
                 self.sampleLength = value_tmp & 0x0F
                 logging.debug('Sample Coding : ' + str(self.sampleCoding))
                 logging.debug('Sample Length : ' + str(self.sampleLength))
             if ch == b'\x44':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
+                data_length = self.readheaderdatalength()
                 if data_length != 2:
                     logging.error('Wrong Trace Header : ' + ch.hex())
                     raise ValueError('Wrong Trace Header')
-                self.cryptoDataLength = self.traceFile.readInt(data_length)
+                self.cryptoDataLength = self.traceFile.readint(data_length)
                 logging.debug('Crypto Data Length : ' + str(self.cryptoDataLength))
             if ch == b'\x45':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
+                data_length = self.readheaderdatalength()
                 if data_length != 1:
                     logging.error('Wrong Trace Header : ' + ch.hex())
                     raise ValueError('Wrong Trace Header')
-                self.titleSpace = self.traceFile.readInt(data_length)
+                self.titleSpace = self.traceFile.readint(data_length)
                 logging.debug('Title Space : ' + ch.hex())
             if ch == b'\x46':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
-                self.globalTraceTitle = self.traceFile.readStr(data_length)
+                data_length = self.readheaderdatalength()
+                self.globalTraceTitle = self.traceFile.readstr(data_length)
                 logging.debug('Global Trace Title : ' + self.globalTraceTitle)
             if ch == b'\x47':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
-                self.description = self.traceFile.readStr(data_length)
+                data_length = self.readheaderdatalength()
+                self.description = self.traceFile.readstr(data_length)
                 logging.debug('Description : ' + self.description)
             if ch == b'\x48':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
+                data_length = self.readheaderdatalength()
                 if data_length != 4:
                     logging.error('Wrong Trace Header : ' + ch.hex())
                     raise ValueError('Wrong Trace Header : ' + ch.hex())
-                self.xAxisOffset = self.traceFile.readInt()
+                self.xAxisOffset = self.traceFile.readint()
                 logging.debug('X-axis Offset : ' + str(self.xAxisOffset))
             if ch == b'\x49':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
-                self.xLabel = self.traceFile.readStr(data_length)
+                data_length = self.readheaderdatalength()
+                self.xLabel = self.traceFile.readstr(data_length)
                 logging.debug('X Label : ' + self.xLabel)
             if ch == b'\x4A':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
-                self.yLabel = self.traceFile.readStr(data_length)
+                data_length = self.readheaderdatalength()
+                self.yLabel = self.traceFile.readstr(data_length)
                 logging.debug('Y Label : ' + self.yLabel)
             if ch == b'\x4B':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
+                data_length = self.readheaderdatalength()
                 if data_length != 4:
                     logging.error('Wrong Trace Header : ' + ch.hex())
                     raise ValueError
-                self.xAxisScale = self.traceFile.readFloat(data_length)
+                self.xAxisScale = self.traceFile.readfloat(data_length)
                 logging.debug('X-axis Scale : ' + str(self.xAxisScale))
             if ch == b'\x4C':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
+                data_length = self.readheaderdatalength()
                 if data_length != 4:
                     logging.error('Wrong Trace Header : ' + ch.hex())
                     raise ValueError
-                self.yAxisScale = self.traceFile.readFloat(data_length)
+                self.yAxisScale = self.traceFile.readfloat(data_length)
                 logging.debug('Y-axis Scale : ' + str(self.xAxisScale))
             if ch == b'\x4D':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
+                data_length = self.readheaderdatalength()
                 if data_length != 4:
                     logging.error('Wrong Trace Header : ' + ch.hex())
                     raise ValueError
-                self.traceOffsetForDisp = self.traceFile.readInt(data_length)
+                self.traceOffsetForDisp = self.traceFile.readint(data_length)
                 logging.debug('Trace Offet For Displying : ' + self.traceOffsetForDisp)
             if ch == b'\x4E':
                 logging.debug('Parsing Trace Header ' + ch.hex())
-                data_length = self.readHeaderDataLength()
+                data_length = self.readheaderdatalength()
                 if data_length != 1:
                     logging.error('Wrong Trace header : ' + ch.hex())
                     raise ValueError
-                self.logScale = self.traceFile.readInt(1)
+                self.logScale = self.traceFile.readint(1)
                 logging.debug('Log Scale : ' + str(self.logScale))
 
-        self.traceFile.closeFile()
+        self.traceFile.closefile()
         return True
 
-    def getTrace(self, index):
+    def gettrace(self, index):
         if index < 0 or index > self.traceNumber - 1:
             logging.error('Wrong Trace Index')
             raise ValueError('Wrong Trace Index')
@@ -262,16 +233,16 @@ class Trace(object):
         traceTitle = ''
         cryptoData = None
         self.traceFile.openfile('rb')
-        self.traceFile.seekFile(self.headerLength + index * (self.titleSpace + self.cryptoDataLength + self.pointCount))
+        self.traceFile.seekfile(self.headerLength + index * (self.titleSpace + self.cryptoDataLength + self.pointCount))
         if self.titleSpace != 0:
-            traceTitle = self.traceFile.readStr(self.titleSpace).decode('utf-8')
+            traceTitle = self.traceFile.readstr(self.titleSpace).decode('utf-8')
             logging.debug('Trace %d title : %s' % (index, traceTitle))
         if self.cryptoDataLength != 0:
-            cryptoData = list(self.traceFile.readByte(self.cryptoDataLength))
+            cryptoData = list(self.traceFile.readbyte(self.cryptoDataLength))
             logging.debug('CryptoData:' + str(cryptoData))
         if self.pointCount != 0:
             if self.sampleCoding == 0:
-                bstr = self.traceFile.readByte(self.sampleLength * self.pointCount)
+                bstr = self.traceFile.readbyte(self.sampleLength * self.pointCount)
                 if self.sampleLength == 1:
                     samplePoint = unpack(str(self.pointCount) + 'B', bstr)
                 elif self.sampleLength == 2:
@@ -279,7 +250,7 @@ class Trace(object):
                 elif self.sampleLength == 4:
                     samplePoint = unpack('<' + str(self.pointCount) + 'I', bstr)
             else:
-                bstr = self.traceFile.readByte(self.sampleLength * self.pointCount)
+                bstr = self.traceFile.readbyte(self.sampleLength * self.pointCount)
                 samplePoint = unpack('<' + str(self.pointCount) + 'f', bstr)
 
         return [traceTitle, cryptoData, samplePoint]
@@ -314,7 +285,7 @@ class Trace(object):
 
         self.traceFile.openFile('wb')
         self.traceFile.writeByte(traceHeader)
-        self.traceFile.closeFileC()
+        self.traceFile.closefileC()
 
     def generateTrace(self, point, cryptoData=None, title=None):
         traceStr = b''
@@ -341,5 +312,5 @@ class Trace(object):
             #     self.traceFile.writeByte(pack('<f', i))
 
         # self.traceFile.writeByte(traceStr)
-        self.traceFile.closeFileC()
+        self.traceFile.closefileC()
 
--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 import logging
 from TraceParser import Trace
+from struct import pack
+import io
+import numpy
 import matplotlib.pyplot as plt
 if __name__ == '__main__':
     logging.basicConfig(filename='logger.log', level=logging.DEBUG)
     #sampleFile =
-    plainFile = open('plain', 'wb')
-    trace = Trace('D:\\trace\\aes\\aes_template.trs')
-    trace.parseTraceHeader()
+    #plainFile = open('plain', 'wb')
+    trace = Trace('../aes_template.trs')
+    trace.parsetraceheader()
+    print('begin')
+    data = trace.gettrace(0)
+    a = numpy.array(data[2], numpy.float32)
+    a = numpy.array()
+    a.tofile('np', )
+    print(a.dtype)
+ #   fwrite = io.FileIO('../sampleData', 'wb+')
+  #  for i in data[2]:
+
+
+   #     fwrite.write(pack('<f', i))
+   # fwrite = io.FileIO('../cryptoData', 'wb+')
+    # for i in range(0, trace.traceNumber):
+    #     data = trace.gettrace(i)
+    #     fwrite.write(bytes(data[1]))
+    #
+    #fwrite.close()
+    print('finish')
+    #cryptoData = open('../cryptoData', 'wb')
+    #cryptoData.write()
     #for i in range(0, trace.traceNumber-1):
-    data = trace.getTrace(0)
-    plainFile.write(data[1])
-
-    plainFile.close()
-
-    plainFile = open('plain', 'r')
-    test = plainFile.read()
-
-
-    #Fig = plt.figure(1)
-    #axes = Fig.add_subplot(111)
-    #axes.hold(False)
     #data = trace.getTrace(0)
-    #axes.plot(data[2])
-    #Fig.show()
-    #Fig.hold()
+   # plainFile.write(data[1])
+
+    #plainFile.close()
+
+    #plainFile = open('plain', 'r')
+    #test = plainFile.read()
+
+
+    # Fig = plt.figure(1)
+    # axes = Fig.add_subplot(111)
+    # axes.hold(False)
+    # data = trace.gettrace(0)
+    # axes.plot(data[2])
+    # Fig.show()
+    # Fig.hold()
